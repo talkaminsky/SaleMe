59d651b7a16c2932fedc0a177d2b0e79f3ed625c
talkaminsky <talkaminsky@gmail.com>
2014-10-03 18:05:12 +0300

    Change to new them

---
+++ public/wp-content/themes/SaleMe/pagebuilder/assets/js/pb-viewb486.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function($){if(jQuery().tabs){$(".block_tabs").tabs({show:true});}
+$('.block_toggle .tab-head, .block_toggle .arrow').each(function(){var toggle=$(this).parent();$(this).click(function(){toggle.find('.tab-body').slideToggle();return false;});});$(function(){$(".block_accordion_wrapper").accordion();$('.ui-accordion-header').disableSelection();});});
